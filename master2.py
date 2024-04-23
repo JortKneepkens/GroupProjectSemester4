d1cd5e607f954dd8b76b0b361e9ad9e59d57e75d
JortKneepkens <153506398+JortKneepkens@users.noreply.github.com>
2024-04-23 10:57:39 +0200

    bpoiahepuitu a89 pgaiiup

--- master2.py
+++ master2.py
@@ -121,83 +121,89 @@ async def main():
     global puzzle
     global user_script_module
     print(f"Connecting to {websocket_uri}...")
-    async with websockets.connect(websocket_uri) as websocket:
-        print("Connected.")
-        # Receive messages until the connection is closed
-        async for message in websocket:
-            try:
-                parsed_message = json.loads(message)
-                message_type = parsed_message.get("Type")
-                message_content = parsed_message.get("Content")
-                if message_type == "File_Uploaded":
-                    print(f"Received message: {message_content}")
-                    local_filename = message_content  # Use the message content as the local filename
-                    if await retrieve_file(ftp_server, ftp_username, ftp_password, message_content, local_filename):
-                        sparkcontext.addFile(local_filename)
-                        # Import the downloaded file as a module
-                        user_script_module = importlib.import_module(local_filename[:-3])  # Remove the ".py" extension
-                        user_script_puzzle = user_script_module.get_puzzle()
-                        puzzle = user_script_puzzle.copy()
+    while True: 
+        try:
+            async with websockets.connect(websocket_uri) as websocket:
+                print("Connected.")
+                # Receive messages until the connection is closed
+                async for message in websocket:
+                    try:
+                        parsed_message = json.loads(message)
+                        message_type = parsed_message.get("Type")
+                        message_content = parsed_message.get("Content")
+                        if message_type == "File_Uploaded":
+                            print(f"Received message: {message_content}")
+                            local_filename = message_content  # Use the message content as the local filename
+                            if await retrieve_file(ftp_server, ftp_username, ftp_password, message_content, local_filename):
+                                sparkcontext.addFile(local_filename)
+                                # Import the downloaded file as a module
+                                user_script_module = importlib.import_module(local_filename[:-3])  # Remove the ".py" extension
+                                user_script_puzzle = user_script_module.get_puzzle()
+                                puzzle = user_script_puzzle.copy()
 
-                        unsolved_tasks = tasks.copy()  # Maintain a copy of unsolved tasks
-                        while unsolved_tasks:
-                            task = unsolved_tasks.pop(0)  # Get the first unsolved task
-                            print("Unsolved tasks remaining:", len(unsolved_tasks))
-                            print (unsolved_tasks)
-                            # Solve task using Spark, passing the current puzzle version
-                            solved_task = sparkcontext.parallelize([task]).map(lambda t: solve_task(t, puzzle)).filter(lambda x: x is not None).collect()[0]
-                            
-                            # Update puzzle
-                            await update_puzzle(solved_task, websocket)
+                                unsolved_tasks = tasks.copy()  # Maintain a copy of unsolved tasks
+                                while unsolved_tasks:
+                                    task = unsolved_tasks.pop(0)  # Get the first unsolved task
+                                    print("Unsolved tasks remaining:", len(unsolved_tasks))
+                                    print (unsolved_tasks)
+                                    # Solve task using Spark, passing the current puzzle version
+                                    solved_task = sparkcontext.parallelize([task]).map(lambda t: solve_task(t, puzzle)).filter(lambda x: x is not None).collect()[0]
+                                    
+                                    # Update puzzle
+                                    await update_puzzle(solved_task, websocket)
 
-                        # Validate the puzzle
-                        valid = user_script_module.is_valid_puzzle(puzzle)
-                        if valid:
-                            print("Sudoku puzzle solved successfully.")
-                            message = {
-                                "Type": "Puzzle_Solved_Success",
-                                "Content": True
-                            }
-                            result = await websocket.send(json.dumps(message))
-                            print(result)
-                            print_puzzle(puzzle)
-                        else:
-                            print("Error: Invalid Sudoku puzzle.")
-                            message = {
-                                "Type": "Puzzle_Solved_Success",
-                                "Content": False
-                            }
-                            result = await websocket.send(json.dumps(message))
-                            print(result)
-                        
-                        # # Delete the file from the FTP server
-                        # await delete_file_from_ftp(ftp_server, ftp_username, ftp_password, message_content)
-                        
-                        # # Unload the module to free up memory
-                        # del sys.modules[local_filename[:-3]]
-                        # importlib.invalidate_caches()
-                        
-                        # # Delete the file from the local filesystem
-                        # os.remove(local_filename)
-                        # # Invoke the cleanup function on each worker
-                        # sparkcontext.parallelize([1]).foreach(lambda x: cloudpickle.loads(serialized_cleanup)(local_filename))
+                                # Validate the puzzle
+                                valid = user_script_module.is_valid_puzzle(puzzle)
+                                if valid:
+                                    print("Sudoku puzzle solved successfully.")
+                                    message = {
+                                        "Type": "Puzzle_Solved_Success",
+                                        "Content": True
+                                    }
+                                    result = await websocket.send(json.dumps(message))
+                                    print(result)
+                                    print_puzzle(puzzle)
+                                else:
+                                    print("Error: Invalid Sudoku puzzle.")
+                                    message = {
+                                        "Type": "Puzzle_Solved_Success",
+                                        "Content": False
+                                    }
+                                    result = await websocket.send(json.dumps(message))
+                                    print(result)
+                                
+                                # # Delete the file from the FTP server
+                                # await delete_file_from_ftp(ftp_server, ftp_username, ftp_password, message_content)
+                                
+                                # # Unload the module to free up memory
+                                # del sys.modules[local_filename[:-3]]
+                                # importlib.invalidate_caches()
+                                
+                                # # Delete the file from the local filesystem
+                                # os.remove(local_filename)
+                                # # Invoke the cleanup function on each worker
+                                # sparkcontext.parallelize([1]).foreach(lambda x: cloudpickle.loads(serialized_cleanup)(local_filename))
 
-            except json.JSONDecodeError as e:
-                print(f"Error decoding JSON: {e}")
-            except Exception as e:
-                print(f"Error processing message: {e}")
-            finally:
-                # Delete the file from the FTP server
-                await delete_file_from_ftp(ftp_server, ftp_username, ftp_password, message_content)
-                
-                # Unload the module to free up memory
-                del sys.modules[local_filename[:-3]]
-                importlib.invalidate_caches()
-                
-                # Delete the file from the local filesystem
-                os.remove(local_filename)
-                # Invoke the cleanup function on each worker
-                sparkcontext.parallelize([1]).foreach(lambda x: cloudpickle.loads(serialized_cleanup)(local_filename))
+                    except json.JSONDecodeError as e:
+                        print(f"Error decoding JSON: {e}")
+                    except Exception as e:
+                        print(f"Error processing message: {e}")
+                    finally:
+                        # Delete the file from the FTP server
+                        await delete_file_from_ftp(ftp_server, ftp_username, ftp_password, message_content)
+                        
+                        # Unload the module to free up memory
+                        del sys.modules[local_filename[:-3]]
+                        importlib.invalidate_caches()
+                        
+                        # Delete the file from the local filesystem
+                        os.remove(local_filename)
+                        # Invoke the cleanup function on each worker
+                        sparkcontext.parallelize([1]).foreach(lambda x: cloudpickle.loads(serialized_cleanup)(local_filename))
+        except Exception as e:
+                print(f"Error connecting to WebSocket server: {e}")
+                print("Retrying...")
+                await asyncio.sleep(10)  # Wacht 5 seconden voordat opnieuw wordt geprobeerd
 
 asyncio.run(main())
 # Stop Spark session
