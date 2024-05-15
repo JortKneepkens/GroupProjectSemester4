145264edd72a59e4547c526759ea1054b6dcf687
JortKneepkens <153506398+JortKneepkens@users.noreply.github.com>
2024-05-15 12:03:46 +0200

    Add files via upload

--- master3.py
+++ master3.py
@@ -140,7 +140,7 @@ async def main():
                             if user_script_module is not None:
                                 # Generate dynamic task chunks based on available workers and network conditions
                                 print("User script module")
-                                tasks = generate_password_tasks(5)
+                                tasks = generate_password_tasks(4)
                                 print("Tasks:")
                                 print(tasks)
                                 # Execute tasks using Spark
