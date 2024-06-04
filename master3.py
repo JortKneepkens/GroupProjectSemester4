628e3250aa6c125929edd910ecb31fe26b38be83
JortKneepkens <153506398+JortKneepkens@users.noreply.github.com>
2024-06-04 17:47:13 +0200

    Add files via upload

--- master3.py
+++ master3.py
@@ -114,7 +114,7 @@ def generate_combinations():
     for length in range(1, max_password_length + 1):
         for combination in itertools.product(CHARACTER_SPACE, repeat=length):
             generated_combination = ''.join(combination)
-            yield generated_combination
+            yield str(generated_combination)
 
 # def generate_chunks(chunk_size, combinations_generator):
 #     chunk = []
